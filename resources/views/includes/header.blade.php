9267bfabe68367f77d150881f311de7f7c9fe346
Stephen <stevekaneya@gmail.com>
2023-09-08 11:52:57 +0300

    Push before re-installing node moduels

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -37,10 +37,16 @@
               <div class="collapse navbar-collapse" id="navbarText">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                   <li class="nav-item">
-                    <a class="nav-link" href="#">Profile</a>
+                    <a class="nav-link" href="/">Current Articles</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
+                    <a class="nav-link" href="#">My Articles </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Add Article </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Profile </a>
                   </li>
                 </ul>
                 @auth
